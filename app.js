e80e8a0837bfe07917210734635e4f51957d1699
ben <benpou2012@gmail.com>
2017-09-17 09:56:45 -0700

    Get address value as a dynamic value in url

---
+++ app.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+        .help()
+        .alias('help', 'h')
+        .argv
+
+console.log(argv);
+
+var encodedAddress = encodeURIComponent(argv.address);
+
+request({
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+    }, (error, response, body) => {
+    if (error) {
+        console.log('There is an ERROR on URL !');
+        return null;
+    };
+
+    var location = {
+        'lat':  body.results[0].geometry.location.lat,
+        'lng': body.results[0].geometry.location.lng
+    }
+    console.log(JSON.stringify(location, undefined, 2));
+
+});
