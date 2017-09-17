9ab5e03e3046f3650a6f3a9aee21607f9663953a
ben <benpou2012@gmail.com>
2017-09-17 11:46:12 -0700

    Move the related code to geocode.js file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,32 +15,9 @@ const argv = yargs
         .alias('help', 'h')
         .argv
 
-console.log(argv);
+geocode.geocodeAddress(argv.address);
 
-var encodedAddress = encodeURIComponent(argv.address);
 
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('There is an ERROR on URL !');
-        return null;
-
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unable to find the address');
-        return null;
-
-    } else if (body.status === 'OK') {
-        var location = {
-            'lat': body.results[0].geometry.location.lat,
-            'lng': body.results[0].geometry.location.lng,
-            'Area': body.results[0].address_components[1].short_name
-        }
-        console.log(JSON.stringify(location, undefined, 2));
-        //console.log(location);
-    }
-});
 
 
 
