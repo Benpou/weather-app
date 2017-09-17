17f1be5201955f23ce7b4bb68d96466fd6093090
ben <benpou2012@gmail.com>
2017-09-17 10:46:58 -0700

    Finish error handeling

--- app.js
+++ app.js
@@ -21,16 +21,28 @@ var encodedAddress = encodeURIComponent(argv.address);
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
-    }, (error, response, body) => {
+}, (error, response, body) => {
     if (error) {
         console.log('There is an ERROR on URL !');
         return null;
-    };
 
-    var location = {
-        'lat':  body.results[0].geometry.location.lat,
-        'lng': body.results[0].geometry.location.lng
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find the address');
+        return null;
+
+    } else if (body.status === 'OK') {
+        var location = {
+            'lat': body.results[0].geometry.location.lat,
+            'lng': body.results[0].geometry.location.lng,
+            'Area': body.results[0].address_components[1].short_name
+        }
+        console.log(JSON.stringify(location, undefined, 2));
+        //console.log(location);
     }
-    console.log(JSON.stringify(location, undefined, 2));
-
 });
+
+
+
+
+
+
